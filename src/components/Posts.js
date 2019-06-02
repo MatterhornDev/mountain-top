fd268423fa24e59122e6c4ac3b902ae81ae24762
Ethan Arrowood <ethan.arrowood@gmail.com>
2019-06-02 18:22:44 -0400

    extrapolate post component

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,6 +3,36 @@ import { StaticQuery, graphql } from 'gatsby'
 // import GatsbyImage from 'gatsby-image'
 import { navigate } from '@reach/router'
 
+export const _Posts = ({data}) => {
+  const { edges } = data.allMarkdownRemark
+  return (
+    <div className='posts'>
+      {edges.map(edge => {
+        const {fields, frontmatter} = edge.node
+        return (
+          <React.Fragment>
+            <hr key={`hr-${fields.slug}`} />
+            <div
+              className='post-container'
+              key={fields.slug}
+              onClick={() => {
+                navigate(`${fields.slug}`)
+              }}
+            >
+              {/* Disabled for now; also disabled in styles. 
+                <GatsbyImage fixed={frontmatter.featuredImage.childImageSharp.fixed} /> */}
+              <div className='post-content'>
+                <h2>{frontmatter.title}</h2>
+                <p>{frontmatter.excerpt}</p>
+              </div>
+            </div>
+          </React.Fragment>
+        )
+      })}
+    </div>
+  )
+}
+
 /*
 This is the old featuredImage query. Currently disabled
 frontmatter {
@@ -38,34 +68,7 @@ const Posts = () => {
           }
         }
       `}
-      render={data => {
-        const { edges } = data.allMarkdownRemark
-        return (
-          <div className='posts'>
-            {edges.map(edge => {
-              const {fields, frontmatter} = edge.node
-              return (
-                <React.Fragment>
-                  <hr key={`hr-${fields.slug}`} />
-                  <div
-                    className='post-container'
-                    key={fields.slug}
-                    onClick={() => {
-                      navigate(`${fields.slug}`)
-                    }}
-                  >
-                    {/* Disabled for now; also disabled in styles. 
-                      <GatsbyImage fixed={frontmatter.featuredImage.childImageSharp.fixed} /> */}
-                    <div className='post-content'>
-                      <h2>{frontmatter.title}</h2>
-                      <p>{frontmatter.excerpt}</p>
-                    </div>
-                  </div>
-                </React.Fragment>
-              )
-            })}
-          </div>
-        )}}
+      render={data => <_Posts data={data}/>}
     />
   )
 }
