e14f7d6cade2723a8f14977d9ce895762d0e2195
Ethan Arrowood <ethan.arrowood@gmail.com>
2019-06-02 12:09:16 -0400

    add react helmet for title

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-transformer-remark`,
       options: {
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Header from './Header'
+import { Helmet } from "react-helmet"
 
 const Layout = ({ headerProps, children }) => {
   return (
     <div className='layout'>
+      <Helmet>
+        <title>Mountain Top</title>
+      </Helmet>
       <Header {...headerProps} />
       {children}
     </div>
